c16aba35b1259258cfb2dbd591765d08aa2e8b7c
BayoussefYassine <yassine.bayoussef97@gmail.com>
2021-04-28 05:48:37 +0000

    router react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import Login from "./Login";
+import Register from "./Register";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Learn</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Learn</h1>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+        <footer>Footer</footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/Login.js
@@ -0,0 +1,17 @@
+
+const Login = () => {
+    return ( 
+        <div className="login">
+            <from>
+                <label htmlFor="">Username</label>
+                <input type="text" />
+                <label htmlFor="">Password</label>
+                <input type="password" />
+
+                <button>Login</button>
+            </from>
+        </div>
+     );
+}
+ 
+export default Login;
