0f04de120fbf776f844e6e4676ee1c4d50808d65
BayoussefYassine <yassine.bayoussef97@gmail.com>
2021-04-28 20:43:11 +0000

    registration page done

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,7 +1,62 @@
+import { Link } from "react-router-dom";
+import logosignup from './img/signup.jpg';
 
 const Register = () => {
     return ( 
-        <h1>Register Page</h1>
+        <div className="register">
+            <h1 className="text-center mt-5 pb-5"><span className="l">L</span>earn</h1>
+
+            <div className="container shadow p-5 rounded mt-5 bg-white">
+                <div className="row">
+
+                    <div className="col-12 col-md-6 mb-5 mb-md-0">
+
+                        <form className=" mx-auto">
+                            <h2 className="my-4">Login</h2>
+                            
+                            <div className="row form-group">
+                                <div className="col">
+                                    <label for="usernmae">Username</label>
+                                    <input type="text" className="form-control" id="username" placeholder="Enter username" />    
+                                </div>
+
+                                <div className="col">
+                                    <label for="usernmae">Email</label>
+                                    <input type="email" className="form-control" id="email" placeholder="Enter email" />    
+                                </div>
+                                                                    
+                            </div>
+                            <div className="row form-group">
+                                <div className="col">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" className="form-control" id="password" placeholder="Password" />  
+                                </div>
+                                
+                                <div className="col">
+                                    <label for="exampleInputPassword1">Confirm Password</label>
+                                    <input type="password" className="form-control" id="confirmPassword" placeholder="Password" />  
+                                </div>
+                            </div>      
+                            <button type="submit" className="btn btn-primary mt-4">Register</button>                            
+                        </form>
+                    </div>
+
+                    <div className="col-12 text-center col-md-6">
+                        <div className="mt-4">
+                            <img src={logosignup} alt="login image"/>
+                        </div>
+                        
+                        <div className="mt-4 create-account">
+                            <Link to="/">Already Member?</Link>
+                        </div>
+                        
+                    </div>
+
+                </div>
+                
+            </div>
+            <div className="test"></div>
+        </div>
      );
 }
  
